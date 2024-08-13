56289f431c45e3bd22b107dc8d082e9519c3ce8c
Admin <hotu2ktb@gmail.com>
2024-08-14 04:20:56 +0700

    Commit note 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 #include <Arduino.h>
 #include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
 #include "Dhole_weather_icons32px.h"
-#include <AnimatedGIF.h>
-#include <Wifi.h>
+
 /*--------------------- DEBUG  -------------------------*/
 #define Sprintln(a) (Serial.println(a))
 #define SprintlnDEC(a, x) (Serial.println(a, x))
@@ -19,7 +18,7 @@
 #define G2_PIN 12
 #define B2_PIN 13
 #define A_PIN 23
-#define B_PIN 22
+#define B_PIN 19
 #define C_PIN 5
 #define D_PIN 17
 #define E_PIN -1
@@ -27,9 +26,11 @@
 #define OE_PIN 15
 #define CLK_PIN 16
 
-#define PANEL_RES_X 64
-#define PANEL_RES_Y 32
-#define PANEL_CHAIN 1
+
+/*--------------------- MATRIX LILBRARY CONFIG -------------------------*/
+#define PANEL_RES_X 64      // Number of pixels wide of each INDIVIDUAL panel module. 
+#define PANEL_RES_Y 32     // Number of pixels tall of each INDIVIDUAL panel module.
+#define PANEL_CHAIN 1      // Total number of panels chained one to another
  
 MatrixPanel_I2S_DMA *dma_display = nullptr;
 
@@ -40,6 +41,37 @@ HUB75_I2S_CFG mxconfig(
 	PANEL_CHAIN    // Chain length
 );
 
+/*
+//Another way of creating config structure
+//Custom pin mapping for all pins
+HUB75_I2S_CFG::i2s_pins _pins={R1, G1, BL1, R2, G2, BL2, CH_A, CH_B, CH_C, CH_D, CH_E, LAT, OE, CLK};
+HUB75_I2S_CFG mxconfig(
+						64,   // width
+						64,   // height
+						 4,   // chain length
+					 _pins,   // pin mapping
+  HUB75_I2S_CFG::FM6126A      // driver chip
+);
+
+*/
+
+
+//mxconfig.gpio.e = -1; // Assign a pin if you have a 64x64 panel
+//mxconfig.clkphase = false; // Change this if you have issues with ghosting.
+//mxconfig.driver = HUB75_I2S_CFG::FM6126A; // Change this according to your pane.
+
+
+/*
+ * Wifi Logo, generated with the following steps: 
+ *
+ * Python and Paint.Net needs to be installed.
+ *
+ * 1. SAVE BITMAP AS 1BIT COLOUR in paint.net
+ * 2. Run: bmp2hex.py -i -x <BITMAP FILE>
+ * 3. Copy paste output into sketch.
+ * 
+ */
+ 
 const char wifi_image1bit[] PROGMEM   =  {
  0x00,0x00,0x00,0xf8,0x1f,0x00,0x00,0x00,0x00,0x00,0x80,0xff,0xff,0x01,0x00,
  0x00,0x00,0x00,0xf0,0xff,0xff,0x07,0x00,0x00,0x00,0x00,0xfc,0xff,0xff,0x1f,
@@ -81,6 +113,7 @@ void drawXbm565(int x, int y, int width, int height, const char *xbm, uint16_t c
 /* Bitmaps */
 int current_icon = 0;
 static int num_icons = 22;
+
 static char icon_name[22][30] = { 
 "cloud_moon_bits",
 "cloud_sun_bits",
@@ -131,9 +164,9 @@ wind_bits};
 
 
 void setup() {
-  delay(1000); 
-  Serial.begin(115200); 
-  delay(200);
+
+  // put your setup code here, to run once:
+  delay(1000); Serial.begin(115200); delay(200);
 
 
   /************** DISPLAY **************/
@@ -142,12 +175,13 @@ void setup() {
   dma_display->begin();
   dma_display->setBrightness8(90); //0-255
   dma_display->clearScreen();
+  
   dma_display->fillScreen(dma_display->color444(0, 1, 0));  
 
   // Fade a Red Wifi Logo In
-  for (int g=0; g < 255; g++ )
+  for (int r=0; r < 255; r++ )
   {
-    drawXbm565(0,0,64,32, wifi_image1bit, dma_display->color565(0,g,0));  
+    drawXbm565(0,0,64,32, wifi_image1bit, dma_display->color565(r,0,0));  
     delay(10);
   }
 
